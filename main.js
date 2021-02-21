d8771e94d57c62692fc35d65d82dd673f2b3b3d4
kyopd0418 <73324806+kyopd0418@users.noreply.github.com>
2021-02-22 00:27:49 +0900

    JS:touch1

--- main.js
+++ main.js
@@ -2,17 +2,16 @@
 
 /* Make navbar transparent when it is on the top */
 const navbar = document.querySelector('#navbar');
-const navbarHeight =  navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log('navbarHeight: ${navbarHeight}');
-  if(window.scrollY > navbarHeight) {
-      navbar.classList.add('navbar--dark');
-  } else {
-      navbar.classList.remove('navbar--dark');
-  }
-
-});
+    console.log(window.scrollY);
+    console.log('navbarHeight: ${navbarHeight}');
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }  
+} );
 
 /* Handle scrolling when tapping on the navbar menu */
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -22,7 +21,14 @@ navbarMenu.addEventListener('click', (event) => {
    if (link == null){
        return;
    }
+   navbarMenu.classList.remove('open');
    scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home 
@@ -67,10 +73,14 @@ workBtnContainer.addEventListener('click', (e) =>{
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
     if(filter == null){
         return;
-    }
-
+    } 
+    //Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+    
     projectContainer.classList.add('anim-out');
-   
     setTimeout(() => {
         projects.forEach((project) => {
             console.log(project.dataset.type);
@@ -83,6 +93,8 @@ workBtnContainer.addEventListener('click', (e) =>{
         projectContainer.classList.remove('anim-out');
     }, 300);
 });
+
+
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
