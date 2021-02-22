55092fb3d149b42f9adbaab2dd17dd87d0e4148a
kyopd0418 <73324806+kyopd0418@users.noreply.github.com>
2021-02-22 15:49:59 +0900

    finaltouch1

--- main.js
+++ main.js
@@ -23,16 +23,17 @@ navbarMenu.addEventListener('click', (event) => {
    }
    navbarMenu.classList.remove('open');
    scrollIntoView(link);
+   selectNavItem(target);
 });
 
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', ()=> {
-    navbarMenu.classList.toggle('open');
-});
+navbarToggleBtn.addEventListener('click', () => {
+        navbarMenu.classList.toggle('open');
+    });
 
 // Handle click on "contact me" button on home 
-const homeContactBtn = document.querySelector('.home__contact')
+const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 });
@@ -95,8 +96,67 @@ workBtnContainer.addEventListener('click', (e) =>{
 });
 
 
+// 1. 모든
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#spanish',
+    '#contact',
+];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => 
+    document.querySelector('[data-link="${id}"]')
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0) 
+        {const index = sectionIds.indexOf('#${entry.target.id}');
+            //스크롤링이 아래로 되어서 페이지가 올라옴
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+ 
+window.addEventListener('wheel', () => {
+    if(window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) 
+    {
+        selectedNavIndex = navItems.length -1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
